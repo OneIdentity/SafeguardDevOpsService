067d854e13d528866243048b48079c30b6a42266
Brad Nicholes <brad.nicholes@quest.com>
2020-07-30 08:33:11 -0600

    Add version numbers to the build

---
+++ DevOpsPluginCommon/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+
+using System.Reflection;
+using System.Resources;
+
+// General Information
+[assembly: AssemblyTitle("Safeguard Secrets Broker for Devops")]
+[assembly: AssemblyDescription("Safeguard Secrets Broker for Devops")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("One Identity LLC")]
+[assembly: AssemblyProduct("SafeguardDevOpsService")]
+[assembly: AssemblyCopyright("Copyright One Identity LLC (2020)")]
+[assembly: AssemblyTrademark("One Identity LLC")]
+[assembly: AssemblyCulture("")]
+
+// Version information(
+[assembly: AssemblyVersion("1.0.0.21")]
+[assembly: AssemblyFileVersion("1.0.0.21")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
+
---
+++ SafeguardDevOpsService/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information
+[assembly: AssemblyTitle("Safeguard Secrets Broker for Devops")]
+[assembly: AssemblyDescription("Safeguard Secrets Broker for Devops")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("One Identity LLC")]
+[assembly: AssemblyProduct("SafeguardDevOpsService")]
+[assembly: AssemblyCopyright("Copyright One Identity LLC (2020)")]
+[assembly: AssemblyTrademark("One Identity LLC")]
+[assembly: AssemblyCulture("")]
+
+// Version information(
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
+
