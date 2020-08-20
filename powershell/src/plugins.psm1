d4513f29c989119c7fdc18e44b95b5e3b2bbb2c9
Brad Nicholes <brad.nicholes@quest.com>
2020-08-20 16:40:21 -0600

    Add documentation to the rest of the cmdlets.  Also added -UseBasicPasing to the Invoke-WebRequest call so that it doesn't fail on Windows machine which don't have IE initialized.

--- powershell/src/plugins.psm1
+++ powershell/src/plugins.psm1
@@ -1,3 +1,27 @@
+<#
+.SYNOPSIS
+Get a list of all registered plugins or a specific plugin by name.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credential information to a specific third party vault. Each plugin must be 
+installed and configured individually.
+
+This cmdlet lists all of the plugins that have been installed along with 
+the specific configuration parameters if invoked with no parameters.  If
+a plugin name is provided, it will return the configuration parameters
+for the specific plugin.
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.EXAMPLE
+Get-SgDevOpsPlugin
+
+.EXAMPLE
+Get-SgDevOpsPlugin -PluginName HashiCorpVault
+
+#>
 function Get-SgDevOpsPlugin
 {
     [CmdletBinding()]
@@ -19,6 +43,37 @@ function Get-SgDevOpsPlugin
     }
 }
 
+<#
+.SYNOPSIS
+Upload and install a new plugin.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credential information to a specific third party vault. Each plugin must be 
+installed and configured individually.
+
+The plugin must be a zip compressed file. The plugin is installed into 
+the \ProgramData\SafeguardDevOpsService\ExternalPlugins folder.
+
+If a new plugin is being installed, restarting the service may not be 
+necessary. However, if an existing plugin is being upgraded, the service
+does not have the ability to unload a loaded plugin. Therefore all plugin
+updates will be installed to a staging folder. The next time that the
+Secrets Broker service is restarted, all staged plugins will be moved
+to the external plugin folder and loaded. To restart automatically after
+installing a plugin, set the restart flag to true.
+
+.PARAMETER PluginZipFile
+The full path and file name of the plugin to be installed.
+
+.PARAMETER Restart
+A boolean that indicates whether the Secrets Broker should be restarted 
+after installing the plugin. 
+
+.EXAMPLE
+Install-SgDevOpsPlugin c:\my\plugin\path\pluginfile.zip
+
+#>
 function Install-SgDevOpsPlugin
 {
     [CmdletBinding()]
@@ -47,6 +102,30 @@ function Install-SgDevOpsPlugin
     }
 }
 
+<#
+.SYNOPSIS
+Delete the configuration for a specific plugin.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credential information to a specific third party vault. Each plugin must be 
+installed and configured individually.
+
+This cmdlet removes the configuration for a specific plugin by name and 
+unregisters the plugin from the Secrets Broker. However, it does not remove 
+the plugin from the \ProgramData\SafeguardDevOpsService\ExternalPlugins 
+folder. The plugin files must be manually removed from the ExternalPlugins 
+folder once Secrets Broker service has been stopped.
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.EXAMPLE
+Remove-SgDevOpsPlugin
+
+.EXAMPLE
+Remove-SgDevOpsPlugin -PluginName HashiCorpVault
+#>
 function Remove-SgDevOpsPlugin
 {
     [CmdletBinding()]
@@ -61,6 +140,24 @@ function Remove-SgDevOpsPlugin
     Invoke-SgDevOpsMethod DELETE "Plugins/$PluginName"
 }
 
+<#
+.SYNOPSIS
+Get a list of accounts that are mapped to a vault plugin.
+
+.DESCRIPTION
+Secrets Broker uses individualized plugins that are capable of pushing 
+credential information to a specific third party vault. Accounts must be 
+mapped to each plugin so that the corresponding credential can be pushed 
+to the third party vault. By mapping an account to a plugin, the Secrets 
+Broker monitor will detect a password change for the mapped account and 
+push the new credential to the plugin.
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.EXAMPLE
+Get-SgDevOpsPluginVaultAccount
+#>
 function Get-SgDevOpsPluginVaultAccount
 {
     [CmdletBinding()]
@@ -75,6 +172,38 @@ function Get-SgDevOpsPluginVaultAccount
     Invoke-SgDevOpsMethod GET "Plugins/$PluginName/VaultAccount"
 }
 
+<#
+.SYNOPSIS
+Map an account with the vault credential to a plugin.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credentials to a specific third party vault. Each plugin usually has a 
+credential that is used to authenticate to the third party vault. This 
+credential must be stored in the Safeguard appliance and fetched at the 
+time when Safeguard Secrets Broker for DevOps needs to authenticate to the 
+third party vault.
+
+The Asset and Account parameters will be used to resolve an asset-account 
+that should be used by the plugin to authenticate to the third party valult.
+This asset-account will be mapped to the plugin and the credential that is
+associated with the asset-account will be pulled from Safeguard at the time
+when the plugin needs to authenticate to the third party vault.
+
+(See get-SgDevOpsAvailableAssetAccount)
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.PARAMETER Asset
+The name of an asset.
+
+.PARAMETER Account
+The name of an account.
+
+.EXAMPLE
+Set-SgDevOpsPluginVaultAccount -PluginName HashiCorpVault -Asset MyVaultAsset -Account MyVaultAccount
+#>
 function Set-SgDevOpsPluginVaultAccount
 {
     [CmdletBinding()]
@@ -96,6 +225,27 @@ function Set-SgDevOpsPluginVaultAccount
     Invoke-SgDevOpsMethod PUT "Plugins/$PluginName/VaultAccount" -Body $local:AssetAccount
 }
 
+<#
+.SYNOPSIS
+Get the settings for a specific plugin.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credentials to a specific third party vault. Each plugin must be installed 
+and configured individually.
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.PARAMETER SettingName
+The name of an plugin setting.
+
+.EXAMPLE
+Get-SgDevOpsPluginSetting -PluginName HashiCorpVault
+
+.EXAMPLE
+Get-SgDevOpsPluginSetting -PluginName HashiCorpVault -SettingName NetworkAddress
+#>
 function Get-SgDevOpsPluginSetting
 {
     [CmdletBinding()]
@@ -120,6 +270,27 @@ function Get-SgDevOpsPluginSetting
     }
 }
 
+<#
+.SYNOPSIS
+Update a setting for a plugin.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credentials to a specific third party vault. Each plugin must be installed 
+and configured individually.
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.PARAMETER SettingName
+The name of an plugin setting.
+
+.PARAMETER SettingValue
+New value for the plugin setting.
+
+.EXAMPLE
+Set-SgDevOpsPluginSetting -PluginName HashiCorpVault -SettingName NetworkAddress -SettingValue 192.168.1.1
+#>
 function Set-SgDevOpsPluginSetting
 {
     [CmdletBinding()]
@@ -141,6 +312,24 @@ function Set-SgDevOpsPluginSetting
     Invoke-SgDevOpsMethod PUT "Plugins/$PluginName" -Body $local:Plugin
 }
 
+<#
+.SYNOPSIS
+Get the list of accounts that are mapped to a vault plugin.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credentials to a specific third party vault. Accounts must be mapped to each 
+plugin so that the corresponding credential can be pushed to the third party 
+vault. By mapping an account to a plugin, the Secrets Broker monitor will 
+detect a password change for the mapped account and push the new credential 
+to the plugin.
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.EXAMPLE
+Get-SgDevOpsMappedAssetAccount -PluginName HashiCorpVault
+#>
 function Get-SgDevOpsMappedAssetAccount
 {
     [CmdletBinding()]
@@ -155,6 +344,46 @@ function Get-SgDevOpsMappedAssetAccount
     Invoke-SgDevOpsMethod GET "Plugins/$PluginName/Accounts"
 }
 
+<#
+.SYNOPSIS
+Map an account or an array of accounts to a vault plugin.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credentials to a specific third party vault. Accounts must be mapped to each 
+plugin so that the corresponding credential can be pushed to the third party 
+vault. By mapping an account to a plugin, the Secrets Broker monitor will 
+detect a password change for the mapped account and push the new credential 
+to the plugin.
+
+The Asset, Account and Domain parameters will be used to resolve an 
+asset-account that will be added to the A2A registration. If an array of
+asset-accounts is provided, the Asset, Account and Domain parameters 
+should omitted.
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.PARAMETER Asset
+The name of an asset.
+
+.PARAMETER Account
+The name of an account.
+
+.PARAMETER Domain
+The name of a domain that the asset-account belong to.
+
+.PARAMETER AccountObjects
+An array of account objects to map to the plugin. 
+(see Get-SgDevOpsRegisteredAssetAccount).
+
+.EXAMPLE
+Add-SgDevOpsMappedAssetAccount -PluginName HashiCorpVault -Asset MyServer -Account MyAccount
+
+.EXAMPLE
+Add-SgDevOpsMappedAssetAccount -PluginName HashiCorpVault -AccountObjects $MyAccounts
+
+#>
 function Add-SgDevOpsMappedAssetAccount
 {
     [CmdletBinding(DefaultParameterSetName="Attributes")]
@@ -190,6 +419,45 @@ function Add-SgDevOpsMappedAssetAccount
     Invoke-SgDevOpsMethod PUT "Plugins/$PluginName/Accounts" -Body $local:NewList
 }
 
+<#
+.SYNOPSIS
+Remove an account or an array of accounts from a vault plugin.
+
+.DESCRIPTION
+The Secrets Broker uses individualized plugins that are capable of pushing 
+credentials to a specific third party vault. Accounts must be mapped to each 
+plugin so that the corresponding credential can be pushed to the third party 
+vault. By mapping an account to a plugin, the Secrets Broker monitor will 
+detect a password change for the mapped account and push the new credential 
+to the plugin.
+
+The Asset, Account and Domain parameters will be used to resolve an 
+asset-account that will be added to the A2A registration. If an array of
+asset-accounts is provided, the Asset, Account and Domain parameters 
+should omitted.
+
+.PARAMETER PluginName
+The name of an installed plugin.
+
+.PARAMETER Asset
+The name of an asset.
+
+.PARAMETER Account
+The name of an account.
+
+.PARAMETER Domain
+The name of a domain that the asset-account belong to.
+
+.PARAMETER AccountObjects
+An array of account objects to unmap from the plugin. 
+(see Get-SgDevOpsMappedAssetAccount).
+
+.EXAMPLE
+Remove-SgDevOpsMappedAssetAccount -PluginName HashiCorpVault -Asset MyServer -Account MyAccount
+
+.EXAMPLE
+Remove-SgDevOpsMappedAssetAccount -PluginName HashiCorpVault -AccountObjects $MyAccounts
+#>
 function Remove-SgDevOpsMappedAssetAccount
 {
     [CmdletBinding(DefaultParameterSetName="Attributes")]
