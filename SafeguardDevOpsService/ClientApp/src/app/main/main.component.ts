2dbb785b352597e766931bdc22f6ccda18bf4b73
JeffHarkavy <jeff.harkavy@quest.com>
2020-10-21 07:14:41 -0400

    work on certificate upload

--- SafeguardDevOpsService/ClientApp/src/app/main/main.component.ts
+++ SafeguardDevOpsService/ClientApp/src/app/main/main.component.ts
@@ -207,6 +207,10 @@ export class MainComponent implements OnInit {
       );
   }
 
+  nukeClientCertificate(): void {
+    this.serviceClient.deleteClientCertificate().subscribe();
+  }
+
   addClientCertificate(e: Event): void {
     e.preventDefault();
 
--- WebsiteUI/ClientApp/src/app/upload-certificate/upload-certificate.component.ts
+++ WebsiteUI/ClientApp/src/app/upload-certificate/upload-certificate.component.ts
@@ -3,6 +3,7 @@ import { DevOpsServiceClient } from '../service-client.service';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { EnterPassphraseComponent } from './enter-passphrase/enter-passphrase.component';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-upload-certificate',
@@ -20,6 +21,7 @@ export class UploadCertificateComponent implements OnInit {
   constructor(
     private serviceClient: DevOpsServiceClient,
     private clipboard: Clipboard,
+    private dialog: MatDialog,
     private dialogRef: MatDialogRef<UploadCertificateComponent>) { }
 
   ngOnInit(): void {
@@ -68,24 +70,78 @@ export class UploadCertificateComponent implements OnInit {
     e.click();
   }
 
+  private saveCert(certData: string, reEnterPassphrase=false) {
+    const dlgData = {enteredPassphrase: '', badPassphrase: reEnterPassphrase};
+    const dialogRefModal = this.dialog.open(EnterPassphraseComponent,
+      { data: dlgData
+      }
+    );
+
+    dialogRefModal.afterClosed().subscribe(result=>{ 
+      if (result != undefined) {
+        this.serviceClient.postClientCertificate(certData, result).subscribe(
+          sslCert => {
+            //this.grid.showLoadingScreen(false);
+            //this.grid.setSelection([sslCert]);
+            //this.grid.refresh();
+          },
+          error => {
+            if (error.error?.Message?.includes('specified network password is not correct')) {
+              // bad password, have another try
+              // error.error?.Code == 60450 is a generic string substitution error, but
+              // it's all we get
+              this.saveCert(certData, true);
+            } else {
+              //this.state = "view";
+              //this.error = error.error;
+              //this.grid.hideLoadingScreen();
+            }
+          }
+        );
+      } else {
+        //this.state = 'view';
+        //this.grid.hideLoadingScreen();
+      }
+    });
+  }
+
   onChangeFile(files: FileList): void {
     if (!files[0]) {
       return;
     }
 
+    //const fileSelected = files[0];
+    //
+    //const reader = new FileReader();
+    //reader.onloadend = (e) => {
+    //const matches = reader.result.toString().match(/^(data.*base64,)(.*)$/);
+    //
+    //this.dialogRef.close({
+    //fileType: fileSelected.type,
+    //fileContents: matches[2],
+    //fileName: fileSelected.name
+    //});
+    //};
+    //
+    //reader.readAsDataURL(fileSelected);
+    /////////////////////////////////////////////////////
     const fileSelected = files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = (e) => {
+      let arrayBufferToString = (buffer) => {
+        var binary = '';
+        var bytes = new Uint8Array( buffer );
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+          binary += String.fromCharCode( bytes[ i ] );
+        }
+        return binary;
+      };
 
-    const reader = new FileReader();
-    reader.onloadend = (e) => {
-      const matches = reader.result.toString().match(/^(data.*base64,)(.*)$/);
-
-      this.dialogRef.close({
-        fileType: fileSelected.type,
-        fileContents: matches[2],
-        fileName: fileSelected.name
-      });
+      var pkcs12Der = arrayBufferToString(fileReader.result);
+      let cert:string = btoa(pkcs12Der);
+      this.saveCert(cert);
     };
-
-    reader.readAsDataURL(fileSelected);
+    fileReader.readAsArrayBuffer(fileSelected);
   }
 }
