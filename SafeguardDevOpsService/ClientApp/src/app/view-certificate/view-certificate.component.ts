a4181e59c52f3e1e0a2ec7bcb04cfa8dfd9ce4a4
JeffHarkavy <jeff.harkavy@quest.com>
2020-10-22 10:51:23 -0400

    - viewing certificate details - moved certificate removal and took out "nuke" button in main.component

--- SafeguardDevOpsService/ClientApp/src/app/view-certificate/view-certificate.component.ts
+++ SafeguardDevOpsService/ClientApp/src/app/view-certificate/view-certificate.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Inject } from '@angular/core';
+import { DevOpsServiceClient } from '../service-client.service';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import * as $ from 'jquery';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-view-certificate',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-certificate.component.scss']
 })
 export class ViewCertificateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  certificateType: string = '';
+  certificateLoaded: boolean = false;
+  retrievedCert = {
   }
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private serviceClient: DevOpsServiceClient,
+    private dialog: MatDialog,
+  ){}
+
+  ngOnInit(): void {
+    this.certificateType = this.data?.certificateType ?? '';
+    this.serviceClient.getClientCertificate().subscribe(
+      cert => {
+        this.retrievedCert = cert;
+        this.retrievedCert.LocalizedValidFrom = moment(cert.NotBefore).format('LLL (Z)') + ' - ' + moment(cert.NotAfter).format('LLL (Z)');
+        this.certificateLoaded = true;
+      },
+      error => {
+        var foo = error;
+      }
+    );
+  }
+
+  removeCertificate(): void {
+    if (this.certificateType == 'Client' && confirm('Deleting this certificate will break things. Press "OK" to continue.')) {
+      this.serviceClient.deleteClientCertificate().subscribe();
+    }
+  }
 }
