2b991f700a92b9224df77d7d070b04ebb01b714c
Brad Nicholes <brad.nicholes@quest.com>
2020-11-06 12:48:57 -0700

    Fix the versioning in the docker images

--- versionnumber.sh
+++ versionnumber.sh
@@ -11,18 +11,18 @@ echo "semVer = $semVer"
 echo "buildId = $buildId"
 
 buildNumber=$(expr $buildId % 65535) # max value for version part on Windows is 65534
-echo "buildNumber = $($local:buildNumber)"
+echo "buildNumber = ${buildNumber}"
 
 versionString="$semVer.$buildNumber"
-templateVersion = "255.255.65534.65534"
+templateVersion="255.255.65534.65534"
 echo "versionString = $versionString"
-echo "templateVersion = $templateVersion"
+echo "templateVersion = ${templateVersion}"
 
 echo "Searching for AssemblyInfo.cs files in '$sourceDir'"
 find $sourceDir -name AssemblyInfo.cs -print0 | while read -d $'\0' file
 do
     echo "Replacing version information in '$file'"
-    sed -i "s/$templateVersion/$versionString" $file
+    sed -i "s/$templateVersion/$versionString/" $file
     echo "*****"
     cat $file
     echo "*****"
