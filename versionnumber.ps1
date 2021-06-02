a78ab1f8e431290e437d4270b43c1960f4d983c3
petrsnd <petrsnd@gmail.com>
2021-06-02 14:18:34 -0600

    fix nuget build and assembly info, move to 2.0

--- versionnumber.ps1
+++ versionnumber.ps1
@@ -5,7 +5,9 @@ Param(
     [Parameter(Mandatory=$true, Position=1)]
     [string]$SemanticVersion,
     [Parameter(Mandatory=$true, Position=2)]
-    [string]$BuildId
+    [string]$BuildId,
+    [Parameter(Mandatory=$true, Position=3)]
+    [bool]$IsPrerelease
 )
 
 Write-Host "SemanticVersion = $SemanticVersion"
@@ -14,20 +16,36 @@ Write-Host "BuildId = $BuildId"
 $local:BuildNumber = ($BuildId % 65535) # max value for version part on Windows is 65534
 Write-Host "BuildNumber = $($local:BuildNumber)"
 
-$local:VersionString = "${SemanticVersion}.$($local:BuildNumber)"
-$local:TemplateVersion = "255.255.65534.65534"
-Write-Host "VersionString = $($local:VersionString)"
-Write-Host "TemplateVersion = $($local:TemplateVersion)"
-
-Write-Host "Searching for AssemblyInfo.cs files in '$SourceDir'"
-(Get-ChildItem -Recurse -Filter "AssemblyInfo.cs") | ForEach-Object {
-    $local:Path = $_.FullName
-    Write-Host "Replacing version information in '$($local:Path)'"
-    (Get-Content $local:Path -Raw).replace($local:TemplateVersion, $local:VersionString) | Set-Content $local:Path
-
-    Write-Output "*****"
-    Get-Content $local:Path
-    Write-Output "*****"
+$local:PackageCodeMarker = "255.255.65534"
+$local:AssemblyCodeMarker = "255.255.65534.65534"
+$local:AssemblyVersion = "${SemanticVersion}.$($local:BuildNumber)"
+if ($IsPrerelease)
+{
+    $local:PackageVersion = "${SemanticVersion}-dev-$($local:BuildNumber)"
 }
+else
+{
+    $local:PackageVersion = $SemanticVersion
+}
+Write-Host "PackageCodeMarker = $($local:PackageCodeMarker)"
+Write-Host "AssemblyCodeMarker = $($local:AssemblyCodeMarker)"
+Write-Host "PackageVersion = $($local:PackageVersion)"
+Write-Host "AssemblyVersion = $($local:AssemblyVersion)"
 
-Write-Output "##vso[task.setvariable variable=VersionString;]$($local:VersionString)"
+Write-Host "Replacing version information in SafeguardDevOpsService assembly info"
+$local:ProjectFile = (Join-Path $PSScriptRoot "SafeguardDevOpsService\Properties\AssemblyInfo.cs")
+(Get-Content $local:ProjectFile -Raw).replace($local:AssemblyCodeMarker, $local:AssemblyVersion) | Set-Content -Encoding UTF8 $local:ProjectFile
+(Get-Content $local:ProjectFile -Raw).replace($local:PackageCodeMarker, $local:PackageVersion) | Set-Content -Encoding UTF8 $local:ProjectFile
+Write-Output "*****"
+Get-Content $local:ProjectFile
+Write-Output "*****"
+
+Write-Host "Replacing version information in DevOpsPluginCommon project file"
+$local:ProjectFile = (Join-Path $PSScriptRoot "DevOpsPluginCommon\DevOpsPluginCommon.csproj")
+(Get-Content $local:ProjectFile -Raw).replace($local:AssemblyCodeMarker, $local:AssemblyVersion) | Set-Content -Encoding UTF8 $local:ProjectFile
+(Get-Content $local:ProjectFile -Raw).replace($local:PackageCodeMarker, $local:PackageVersion) | Set-Content -Encoding UTF8 $local:ProjectFile
+Write-Output "*****"
+Get-Content $local:ProjectFile
+Write-Output "*****"
+
+Write-Output "##vso[task.setvariable variable=VersionString;]$($local:AssemblyVersion)"
