cb5489a9c662fa0c221573bd075336e15d6c8115
petrsnd <petrsnd@gmail.com>
2021-07-08 14:05:15 -0600

    Modifying the cleanup script to be aware of devops

--- clear-secretsbrokers.ps1
+++ clear-secretsbrokers.ps1
@@ -16,21 +16,25 @@ if (-not $SafeguardSession)
     throw "This script requires a Safeguard session.  Please login using Connect-Safeguard."
 }
 
+$local:DevOps = (Invoke-SafeguardMethod Core GET DevOps/SecretsBrokers)
+Write-Host ($local:DevOps | Format-Table Id,AssetId,AssetName | Out-String)
+Write-Host "Cleaning up DevOps entries:"
+Write-host ($local:DevOps | Format-Table | Out-String)
+$local:DevOps | ForEach-Object {
+    Invoke-SafeguardMethod Core DELETE "DevOps/SecretsBrokers/$($_.Id)"
+}
+
 $local:Filter = "PrimaryAuthenticationProviderName eq 'Certificate' and Description eq 'Safeguard User for Safeguard Secrets Broker for DevOps'"
 $local:A2aUsers = (Find-SafeguardUser -QueryFilter $local:Filter -Fields Id,UserName,PrimaryAuthenticationIdentity)
-
 Write-Host "Cleaning up A2A certificate users:"
 Write-host ($local:A2aUsers | Format-Table | Out-String)
-
 $local:A2aUsers | ForEach-Object {
     Remove-SafeguardUser $_
 }
 
 $local:A2as = ((Get-SafeguardA2a) | Where-Object { $_.AppName -match "SafeguardDevOpsService*" })
-
 Write-Host "Cleaning up A2A registrations:"
-Write-host ($local:A2as | Format-Table | Out-String)
-
+Write-host ($local:A2as | Format-Table Id,AppName | Out-String)
 $local:A2as | ForEach-Object {
     Remove-SafeguardA2a $_
 }
